540f916d509320b98e902e017c1ea7134be3869e
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2025-05-16 10:44:18 +0600

    added Link on CLick on Logo

--- src/app/(website)/_components/navbar.tsx
+++ src/app/(website)/_components/navbar.tsx
@@ -8,12 +8,21 @@ const LandingPageNavbar = () => {
     <nav className="flex w-full justify-between items-center">
       <div className="text-3xl font-semibold flex items-center gap-x-3">
         <Menu className="w-8 h-8" />
-        <Image alt="KoomAi LOGO" src="/logo.png" width={120} height={50} />
+        <Link href={"/"}>
+          <Image
+            alt="KoomAi LOGO"
+            src="/logo.png"
+            width={120}
+            height={50}
+            className="cursor-pointer"
+          />
+        </Link>
       </div>
       <div className="hidden lg:flex justify-center items-center gap-x-10 ">
         <Link
           href={"/"}
-          className="bg-[#7320DD] py-2 px-5 font-semibold text-lg rounded-full hover:bg-[#7320DD]/80"
+          className="bg-[#7320DD] py-2 px-5 font-semibold 
+          text-lg rounded-full hover:bg-[#7320DD]/80"
         >
           Home
         </Link>
