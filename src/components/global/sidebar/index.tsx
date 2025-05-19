f0b5eff75e1f81b02690665c6e366727f1e61834
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2025-05-19 09:49:40 +0600

    select a workspace -> code added

--- src/components/global/sidebar/index.tsx
+++ src/components/global/sidebar/index.tsx
@@ -1,15 +1,53 @@
+"use client";
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Separator } from "@/components/ui/separator";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 type Props = {
   activeWorkSpaceId: string;
 };
 
 const Sidebar = ({ activeWorkSpaceId }: Props) => {
+  const router = useRouter();
+
+  const onChangeActiveWorkSpace = (value: string) => {
+    router.push(`/dashboard/${value}`);
+  };
+
   return (
     <div className="bg-[#111111] flex-none relative p-4 h-full w-[250px] flex flex-col gap-4 items-center overflow-hidden">
       <div className="bg-[#111111] p-4 gap-2 justify-center items-center mb-4 absolute top-0 left-0 right-0">
-        <Image src={"/logo.png"} height={100} width={56} alt="logo" />
-        Sidebar : {activeWorkSpaceId}
+        <Image
+          src={"/logo.png"}
+          height={100}
+          width={100}
+          className="ml-6"
+          alt="logo"
+        />
+        <Select
+          defaultValue={activeWorkSpaceId}
+          onValueChange={onChangeActiveWorkSpace}
+        >
+          <SelectTrigger className="mt-4 text-neutral-400 bg-transparent">
+            <SelectValue placeholder="Select a workspace"></SelectValue>
+          </SelectTrigger>
+          <SelectContent className="bg-[#111111] backdrop-blur-xl">
+            <SelectGroup>
+              <SelectLabel>WorkSpaces</SelectLabel>
+              <Separator />
+              {}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
       </div>
     </div>
   );
