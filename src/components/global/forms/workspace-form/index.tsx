62873591b86ab18d675f77971eb81f3b6b2a5133
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2025-05-25 00:54:57 +0600

    (feat): Form Creation, handling useCreateWorkspace, schema

--- src/components/global/forms/workspace-form/index.tsx
+++ src/components/global/forms/workspace-form/index.tsx
@@ -1,5 +1,13 @@
-const WorkspaceForm = () => {
-  return <div>WorkspaceForm</div>;
+import { useCreateWorkspace } from "@/hooks/useCreateWorkspace";
+type Props = {};
+
+const WorkspaceForm = (props: Props) => {
+  const { errors, isPending, onFormSubmit, register } = useCreateWorkspace();
+  return (
+    <form onSubmit={onFormSubmit} className="flex flex-col gap-y-3">
+      Kishor
+    </form>
+  );
 };
 
 export default WorkspaceForm;
--- src/hooks/useCreateWorkspace.tsx
+++ src/hooks/useCreateWorkspace.tsx
@@ -1,13 +1,19 @@
 import { createWorkspace } from "@/actions/workspace";
+import { workspaceSchema } from "@/components/global/forms/workspace-form/schema";
 import { useMutationData } from "./useMutationData";
-import { useZodForm } from "./useZodForm";
+import useZodForm from "./useZodForm";
 
-export const useCreateWorkspace = async () => {
-  const {} = useMutationData(
+export const useCreateWorkspace = () => {
+  const { mutate, isPending } = useMutationData(
     ["create-workspace"],
     (data: { name: string }) => createWorkspace(data.name),
     "user-workspaces"
   );
 
-  const {} = useZodForm();
+  const { errors, onFormSubmit, register } = useZodForm(
+    workspaceSchema,
+    mutate
+  );
+
+  return { errors, onFormSubmit, register, isPending };
 };
