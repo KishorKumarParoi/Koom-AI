7421bf798cd0c61994e9fae4eab1167d60d5e09e
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2025-07-02 22:52:50 +0600

    feat: add and style Spinner component for loading states
    - Implement customizable SVG spinner for global loader
    - Improve loading UX in create-workspace and other

--- src/components/global/create-workspace/index.tsx
+++ src/components/global/create-workspace/index.tsx
@@ -42,7 +42,10 @@ const CreateWorkSpace = () => {
         title="Create a Workspace"
         description="Workspace helps you collaborate with your team members. You are assigned a deafault personal workspace where you can share videos in private with yourself"
         trigger={
-          <Button className="bg-[#1D1D1D] text-[#c9c1c1] hover:text-black hover:bg-[#d2cbcb] flex items-center cursor-pointer gap-2 py-6 px-4 rounded-2xl ">
+          <Button
+            className="bg-[#1D1D1D] text-[#c9c1c1] hover:text-black hover:bg-[#d2cbcb] flex items-center cursor-pointer gap-2 py-6 px-4 rounded-2xl "
+            suppressHydrationWarning
+          >
             <FolderPlusIcon />
             Create Workspace
           </Button>
