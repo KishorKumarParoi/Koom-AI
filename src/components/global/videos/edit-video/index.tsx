1c2aeb57b315c37b5852303ba37b81c5894344af
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2025-08-30 23:56:54 +0600

    feat: improve edit-video component structure

--- src/components/global/videos/edit-video/index.tsx
+++ src/components/global/videos/edit-video/index.tsx
@@ -1,11 +1,29 @@
+import { Button } from "@/components/ui/button";
+import { Edit } from "lucide-react";
+import Modal from "../../modal";
+
 type Props = {
   videoId: string;
   title: string;
   description: string;
 };
+
 const EditVideo = (props: Props) => {
   const { videoId, title, description } = props;
-  return <div>EditVideo</div>;
+
+  return (
+    <Modal
+      title="Edit video details"
+      description="You can update your video details here"
+      trigger={
+        <Button className="cursor-pointer">
+          <Edit className="text-[#6c6c6c] size-6 " />
+        </Button>
+      }
+    >
+      EditVideo
+    </Modal>
+  );
 };
 
 export default EditVideo;
